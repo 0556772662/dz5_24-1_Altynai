f7968f81c4b45b3f856e46270f0c756fcc041afc
Altynai <kenebaevaaltynai@gmail.com>
2023-04-26 13:40:18 +0600

    homework_5

---
+++ app.js
@@ -0,0 +1,36 @@
+const som = document.querySelector('#som')
+const usd = document.querySelector('#usd')
+const euro = document.querySelector('#euro')
+
+
+const convert = (elem, target, target2) => {
+    elem.oninput = () => {
+        const request = new XMLHttpRequest()
+        request.open("GET", "data.json")
+        request.setRequestHeader("Content-type", "application/json")
+        request.send()
+
+        request.onload = () => {
+            const response = JSON.parse(request.response)
+            target.forEach(function (e) {
+                if (target2 === 'som') {
+                    e.value = (elem.value / response[e.id]).toFixed(2)
+                } else if (e === som) {
+                    e.value = (elem.value * response[elem.id]).toFixed(2)
+                } else {
+                    e.value = ((elem.value * response[elem.id]) / response[e.id]).toFixed(2)
+                }
+            })
+            elem.value === '' && (target.forEach(e => e.value = ''))
+        }
+        request.onerror = () => {
+            console.error('Error')
+        }
+    }
+}
+convert(som, [usd,euro])
+convert(usd, [som,euro])
+convert(euro, [som,usd])
+
+
+
